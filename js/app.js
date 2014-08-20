22d92d6b653dda75a2ca7f8de6874d0f89df0140
Ken <nyhunter77@yahoo.com>
2014-08-20 12:47:30 -0400

    enter submits twice ?
    maybe I'll move on to the logic and come back to this later.

--- js/app.js
+++ js/app.js
@@ -9,9 +9,42 @@ $(document).ready(function(){
 
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
+  	$(".overlay").fadeOut(1000);
   	});
+	
+	/*--- Get the hidden number and start the game ---*/
+	$(".new").click(function() {
+		answer=Math.floor((Math.random()*100)+1);
+		console.log (answer);
+	});
 
+	/*--- Take in guess ---*/
+	function getGuess() {
+		$("#guessButton").click(function(enter) {
+				console.log("submit button");
+				game();
+		});
+		$("#userGuess").keydown(function(enter) {
+			if (enter.keyCode===13) {
+				console.log("Enter was pressed");
+				game();
+			};
+		}); 
+	};
+
+	getGuess();
+
+	/*--- Play the game   Need to take in the var guess---*/
+	
+	function game() {
+		var guess = $("#userGuess").val();
+		
+		if (guess==answer) {
+			alert ("Congratulations! You got it! The secret number was " +answer+ "!")
+		} else {
+			alert ("wrong");	
+		};
+	};
 });
 
 
