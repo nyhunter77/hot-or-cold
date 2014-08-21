a3584c6486d6b62a9e76a7347082ca0164ec0133
nyhunter77 <nyhunter77@yahoo.com>
2014-08-21 12:14:07 -0400

    figuring out the logic of hot cold

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 
 $(document).ready(function(){
+	/*--- Get the number at web page load ---*/
+	console.log("ready");
+	answer=Math.floor((Math.random()*100)+1);
+		console.log (answer);
+	
+	/*--- Get the hidden number and start the game on new button ---*/
+	$(".new").click(function() {
+		answer=Math.floor((Math.random()*100)+1);
+		console.log (answer);
+	});	
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -11,12 +21,6 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   	$(".overlay").fadeOut(1000);
   	});
-	
-	/*--- Get the hidden number and start the game ---*/
-	$(".new").click(function() {
-		answer=Math.floor((Math.random()*100)+1);
-		console.log (answer);
-	});
 
 	/*--- Take in guess ---*/
 	function getGuess() {
@@ -36,16 +40,50 @@ $(document).ready(function(){
 
 	getGuess();
 
-	/*--- Play the game   Need to take in the var guess---*/
+	/*--- Play the game ---*/
 	
 	function game() {
 		var guess = $("#userGuess").val();
+		var howfar=Math.abs(answer-guess);
+		var guesses=[];
+		guesses.push(guess);
+		var prevhowfar=Math.abs(answer-guesses);
 		
 		if (guess==answer) {
 			console.log ("Congratulations! You got it! The secret number was " +answer+ "!")
 		} else {
-			console.log ("wrong");	
+			
+			if (isNaN(prevhowfar)) {
+				if (guess>answer) {
+					console.log ("You're cold, guess lower.")					
+				} else if (guess<answer) {
+					console.log ("You're cold, guess higher.")
+				}	
+			} else if (howfar>prevhowfar) {  //colder
+				if (guess>answer) {
+					console.log ("Yikes! You're really starting to freeze! Guess lower!")
+				} else if (guess<answer) {
+					console.log ("Yikes! You're really starting to freeze! Guess higher!")
+				}
+			} else if (howfar<prevhowfar) {  //warmer
+				if (guess>answer) {
+					console.log ("Ok, you're getting warmer ... guess a little lower.")
+				} else if (guess<answer) {
+					console.log ("Ok, you're getting warmer ... guess a little higher.")
+				}
+			} else {
+			
+				console.log ("Sorry, that's not it.");
+			};
 		};
+		
+		
+	//if guess = or not  / do we have a prevhowfar?
+	//another guess?  need distance, if less = warmer / if more = colder
+	//warmer - take the absolute value as distance. howfar=absolute(answer-guess)
+	//if distance is greater - getting colder / if distance is getting lesser - getting warmer.
+	//colder - if distance is greater - getting colder - etc
+
 	};
 });
 
